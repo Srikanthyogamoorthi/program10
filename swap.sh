8015f4153969505502024103c79f4459ff4b1235
srikanth <srikanthy476@gmail.com>
2026-09-23 12:42:13 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# =====================================
-# Swap Space Creation Script
-# Student Name:
-# Roll Number:
-# =====================================
+# Removes existing swapfile
+rm -f /swapfile
 
-# Write your commands below
+# Creates swap file
+dd if=/dev/zero of=/swapfile bs=1M count=1024
 
+# Uses chmod 600
+chmod 600 /swapfile
 
+# Creates swap signature
+mkswap /swapfile
+
+# Activates swap
+swapon /swapfile
